f11dcc3ad2186df8f23cb7a0cd36e23bbe5fef2a
Anna <34191811+404unf@users.noreply.github.com>
2023-06-09 22:29:45 -0700

    Adding event listener

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function () {
+    const getText = document.getElementById('text')
+    getText.textContent = "This is really cool!";
+});
+
+
+
